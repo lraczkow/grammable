6c8035904f9aa43d4e0af5724d0e4e499517be92
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-24 18:23:04 +0000

    adding create functions

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#create action" do
+    it "should successfully create a new gram in our database" do
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
+
+      gram = Gram.last
+      expect(gram.message).to eq("Hello!")
+    end
+  end
 end
