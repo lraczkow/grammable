2ef87f1aa29eaf2ed891f8a94f3fbdf712ccc98d
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-24 18:14:08 +0000

    creating new gram action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
