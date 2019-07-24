334d8af32e78193532c7742fbc0ad82483843f4f
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-24 18:33:12 +0000

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = {host: 'https://grammable-lauren-raczkoski.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
