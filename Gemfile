cadcce755d667918a7000a3344100ba9ea5c98be
Lauren Raczkowsk <lracz117@gmail.com>
2019-08-01 20:34:38 +0000

    carrierwave

--- Gemfile
+++ Gemfile
@@ -76,4 +76,6 @@ end
 
 gem 'simple_form'
 gem 'devise'
-
+gem 'carrierwave', '>= 2.0.0.rc', '< 3.0'
+gem "figaro"
+gem "fog-aws"
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,4 +1,5 @@
 class Gram < ApplicationRecord
+  mount_uploader :picture, PictureUploader
   validates :message, presence: true
 
   belongs_to :user
