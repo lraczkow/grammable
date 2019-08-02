9a85ec983eb4c8192a4971248e25881013a26668
Lauren Raczkowsk <lracz117@gmail.com>
2019-08-02 17:34:02 +0000

    comments

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,7 @@ class Gram < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
 
   belongs_to :user
 end
